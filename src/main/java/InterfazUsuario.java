a2a68a5a5a87d71d26fee298260061a0d2097570
Egly Veloz <eveloz8324@utm.edu.ec>
2023-10-28 21:45:09 -0500

    Commit 4

--- src/main/java/InterfazUsuario.java
+++ src/main/java/InterfazUsuario.java
@@ -1,13 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.util.Scanner;
 
-/**
- *
- * @author LENOVO
- */
 public class InterfazUsuario {
-    
+    private Scanner inputScanner;
+
+    public InterfazUsuario() {
+        this.inputScanner = new Scanner(System.in);
+    }
+
+    public void mostrarPregunta(String question) {
+        System.out.print(question + ": ");
+    }
+
+    public int obtenerRespuestaUsuario() {
+        return inputScanner.nextInt();
+    }
 }
