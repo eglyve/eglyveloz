4dfc8c018b3bcfc017518ab84dba7812a026b2f5
Egly Veloz <eveloz8324@utm.edu.ec>
2023-10-28 21:39:59 -0500

    Commit 2

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -6,7 +6,12 @@ public class MainClass {
         Scanner inputScanner = new Scanner(System.in);
         Random rng = new Random();
 
-        
+         System.out.print("Selecciona el nivel (1=sencillo, 2=medio,): ");
+        int difficulty = inputScanner.nextInt();
+
+        System.out.print("Tipo de ejercicio (1=Suma, 2=Resta, 3=Multiplicación, 4=División, 5=Random): ");
+        int problemType = inputScanner.nextInt();
+       
     }
     
 }
