79e3378239c480a81f7df811f125964167e46715
Egly Veloz <eveloz8324@utm.edu.ec>
2023-10-28 21:34:44 -0500

    Commit 1

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Scanner inputScanner = new Scanner(System.in);
+        Random rng = new Random();
+
+        
+    }
+    
+}
