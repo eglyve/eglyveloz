9fc8b4c1d004b53716c93d41d66f90e172987025
Egly Veloz <eveloz8324@utm.edu.ec>
2023-10-28 21:46:06 -0500

    Commit 5

--- src/main/java/GeneradorPreguntas.java
+++ src/main/java/GeneradorPreguntas.java
@@ -1,13 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.security.SecureRandom;
 
-/**
- *
- * @author LENOVO
- */
 public class GeneradorPreguntas {
-    
+    private SecureRandom randomGen;
+    private int difficulty;
+    private int problemType;
+
+    // El resto del código aquí...
 }
+
